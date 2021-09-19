38b4f472cde58c91be9be9af085d384c309c6183
Hiran Saravanan <hiranvikraman2001sr@gmail.com>
2021-09-19 18:21:46 +0530

    Sign-up and Password

--- bot.py
+++ bot.py
@@ -6,5 +6,19 @@ bot = telebot.TeleBot(confi.telegramapi, parse_mode=None) # You can set parse_mo
 def send_welcome(message):
 	bot.reply_to(message, "Hi, how are you doing?")
 
+#Sign-up and Password
+@bot.message_handler(commands=['Signup'])
+def signup(message):
+    db.insertdata(message.chat.id,"chatid","signup")
+    msg = bot.send_message(message.chat.id, "Enter User Name: ")
+    bot.register_next_step_handler(msg, username) #spass= sign password
+def username(message):
+    db.updatedata(message.chat.id,"username",message.text,"signup")
+    bot.send_message(message.chat.id,f"Your Username is {message.text}")
+    msg = bot.send_message(message.chat.id, "Enter Password: ")
+    bot.register_next_step_handler(msg, password) 
+def password(message):
+    db.updatedata(message.chat.id,"pass",message.text,"signup")
+    bot.send_message(message.chat.id,"Signup Successful")
 
 bot.polling()
