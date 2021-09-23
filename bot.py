7f23c7094d8a1ba4fb062a0970d08925df506928
Hiran Saravanan <hiranvikraman2001sr@gmail.com>
2021-09-23 19:05:58 +0530

    Signup and Login(without db)

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
 import telebot
 import db
 import confi
-bot = telebot.TeleBot(confi.telegramapi, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+import pyotp
+import qrcode
+from PIL import Image
+bot = telebot.TeleBot(confi.telegramapi, parse_mode="HTML") # You can set parse_mode by default. HTML or MARKDOWN
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hi, how are you doing?")
+	bot.reply_to(message, "Hlo")
 
 #Sign-up and Password
 @bot.message_handler(commands=['Signup'])
 def signup(message):
+    if(db.checkdata(message.chat.id,"chatid")):
+        bot.send_message(message.chat.id,"User Already exist")
+        bot.send_message(message.chat.id,f"Your username is {db.getfield(message.chat.id,'username')}")
+        bot.send_message(message.chat.id, "Login")
+        msg = bot.send_message(message.chat.id, "Enter Your UserName: ")
+        bot.register_next_step_handler(msg,login)
+        return
     db.insertdata(message.chat.id,"chatid","signup")
     msg = bot.send_message(message.chat.id, "Enter User Name: ")
     bot.register_next_step_handler(msg, username) #spass= sign password
@@ -19,6 +29,42 @@ def username(message):
     bot.register_next_step_handler(msg, password) 
 def password(message):
     db.updatedata(message.chat.id,"pass",message.text,"signup")
-    bot.send_message(message.chat.id,"Signup Successful")
-
+    key=pyotp.random_base32()    #TOTP
+    db.updatedata(message.chat.id,"totp",key,"signup")
+    bot.send_message(message.chat.id,f"Totp for 2Factor Authentication is <code>{key}</code>") #<code> to select
+    ######## QR code
+    tt = pyotp.totp.TOTP(key).provisioning_uri(name="Hiran", issuer_name='Crypto Cham')
+    img = qrcode.make(key)
+    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=5, border=4,)
+    qr.add_data(tt)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+    bot.send_photo(message.chat.id, img, f"<code>{key}</code>")
+    #################
+    msg = bot.send_message(message.chat.id, "Enter 2FA Code: ")
+    bot.register_next_step_handler(msg,checkotp)
+def checkotp(message):
+    totp = pyotp.TOTP(db.getfield(message.chat.id,"totp"))
+    if(totp.verify(message.text)):
+        bot.send_message(message.chat.id,"Signup Successful")
+        bot.send_message(message.chat.id, "Login")
+        msg = bot.send_message(message.chat.id, "Enter Your UserName: ")
+        bot.register_next_step_handler(msg,login)
+    else:
+        msg = bot.send_message(message.chat.id, "Invalid Otp \nEnter again: ")
+        bot.register_next_step_handler(msg,checkotp)
+def login(message):
+    if(db.checkdata(message.text,"username")):
+        msg = bot.send_message(message.chat.id, "Enter Your Password: ")
+        bot.register_next_step_handler(msg,passw)
+    else:
+        msg = bot.send_message(message.chat.id, "Invalid UserName and Password \nEnter Your UserName again: ")
+        bot.register_next_step_handler(msg,login)
+def passw(message):
+    if(db.checkdata(message.text,"pass")):
+        db.insertdata(message.chat.id,"chatid","signup")
+        bot.send_message(message.chat.id,"Login Successful")
+    else:
+        msg=bot.send_message(message.chat.id,"Incorrect Username and Password \nEnter again: ")
+        bot.register_next_step_handler(msg,login)
 bot.polling()
--- db.py
+++ db.py
@@ -1,18 +1,36 @@
 import psycopg2
 import confi
+#connect
 conn = psycopg2.connect(
     host="localhost",
     database="postgres",
     user="postgres",
     password=confi.dbpass)
 cur = conn.cursor()
+#insert new row
 def insertdata(data, field, table):
   sql = f"INSERT INTO {table} ({field}) VALUES (%s)"
   val = (data,)
   cur.execute(sql, val)
   conn.commit()
+#update the same row
 def updatedata(cid, cname, data, table):
-  sql = f"UPDATE {table} SET {cname}=(%s) where chatid=(%s);"
-  val = (data, cid)
+  sql = f"UPDATE {table} SET {cname}=(%s) where chatid=(%s);" #%s=data,%s=cid
+  val = (data, cid)# converting into tuple for sql injection
   cur.execute(sql, val)
   conn.commit()
+#Verify the data
+def checkdata(data, field):
+  uval = (data,) #to convert it into tuple becoz one data is not considered as tuple
+  sql = f"select * from signup where {field}=%s" #%s=data
+  cur.execute(sql, uval) 
+  data = cur.fetchall()
+  if(len(data) == 0):
+    return 0
+  return 1
+# display field if already exist
+def getfield(data,field):
+  sql=f"select {field} from signup where chatid={data}"
+  cur.execute(sql)
+  data = cur.fetchall()
+  return data[0][0]
